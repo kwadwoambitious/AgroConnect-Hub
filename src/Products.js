44d243d6686e714e2637a5602e6e10ce08ff62b1
Antwi Ebenezer <antwiebenezer824@gmail.com>
2024-06-11 11:56:19 +0000

    Added new object elements

---
+++ src/Products.js
@@ -0,0 +1,86 @@
+// products.js
+
+const Products = [
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Tomatoes",
+    quantity: '3 pieces',
+    price: 'GHS 5.00',
+    location: "Kumasi"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Cucumber",
+    quantity: 3,
+    price: 1299.99,
+    location: "Los Angeles"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Beans",
+    quantity: 10,
+    price: 129.99,
+    location: "Chicago"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Pepper",
+    quantity: 2,
+    price: 499.99,
+    location: "Miami"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Onions",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Plantain",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Banana",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Apple",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Cassava",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+  {
+    image: 'src/assets/images/cucumber.webp',
+    alt: 'cucumber image',
+    name: "Cassava Dough",
+    quantity: 4,
+    price: 1799.99,
+    location: "San Francisco"
+  },
+];
+
+export default Products;
